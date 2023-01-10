4fe1e1ac68716657ed6590394ef66173a60438a2
cherif sassi <cherif.sassi@outlook.fr>
2023-01-10 09:16:42 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Button, Form, Row, Col, Container, Nav, Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BasicExample,  InputSizesExample} from './Form'
+
+function App() {
+  return (
+    <div>
+      <BasicExample/>
+      <InputSizesExample/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Form.js
@@ -0,0 +1,60 @@
+import { Button, Form } from 'react-bootstrap';
+
+function BasicExample() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+
+
+
+
+
+
+
+function InputSizesExample() {
+  return (
+    <>
+      <Form.Control size="lg" type="text" placeholder="Large text" />
+      <br />
+      <Form.Control type="text" placeholder="Normal text" />
+      <br />
+      <Form.Control size="sm" type="text" placeholder="Small text" />
+    </>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+export {BasicExample,
+
+InputSizesExample
+}
